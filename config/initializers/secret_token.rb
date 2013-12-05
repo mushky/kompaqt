fd7c10727bdb14bd0a6fda2b6b06548f3f8821f2
mushky <msazaidi@gmail.com>
2013-12-04 18:46:50 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Kompaqt::Application.config.secret_key_base = '725e8badc043779470c8c21b0929d4c042213f7d35aa488d1613352cf85159ddbcc635903d49b408fffc4300e225ce08b1e2df691072037528966948e38298e7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Kompaqt::Application.config.session_store :cookie_store, key: '_kompaqt_session'
