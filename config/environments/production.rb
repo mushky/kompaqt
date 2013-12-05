df8a63f62430648fe46b6fa4f8c709a6b982dd29
mushky <msazaidi@gmail.com>
2013-12-04 21:59:41 -0800

    Add Devise Gem and User Signup

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,7 @@ Kompaqt::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  # Heroku Configuration
+  config.action_mailer.default_url_options = { :host => 'kompaqt.herokuapp.com' }
 end
